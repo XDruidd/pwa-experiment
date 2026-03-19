cd17fc3d58b00b856f4c1ee185b30e28d56c42bb
XDruidd <137764566+XDruidd@users.noreply.github.com>
2026-03-19 17:08:43 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA, type ManifestOptions} from "vite-plugin-pwa";
+
+const manifest : Partial<ManifestOptions> | false = {"theme_color":"#383838","background_color":"#ffffff","icons":[{"purpose":"maskable","sizes":"512x512","src":"icon512_maskable.png","type":"image/png"},{"purpose":"any","sizes":"512x512","src":"icon512_rounded.png","type":"image/png"}],"orientation":"any","display":"standalone","lang":"en","name":"TestApp","short_name":"TA"}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+    VitePWA(
+    {
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ["**/*{html,css,js,ico,png,svg}"]
+      },
+      manifest: manifest
+    },
+  )],
+})
